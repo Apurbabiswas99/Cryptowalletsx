4e0d615648a758961cf6133e61fae205e330a98c
airdropking1810 <85541670+airdropking1810@users.noreply.github.com>
2021-06-08 11:07:29 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -141,16 +141,16 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
+                                                                                   InlineKeyboardButton(text="🧑‍💻Our Channel🧑‍💻",
+                                                                       url="t.me/moneydropos")],
+                                                                                   [InlineKeyboardButton(text="ADD AMELIA HELP BOT TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                                   InlineKeyboardButton(text="Subscribe YouTube Channel For Updates",
+                                                                       url="https://www.youtube.com/channel/UCFHhpxodtEnE0LEeKVJkZrw")
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Yuss, I am Already ONline")
+        update.effective_message.reply_text("Yes, I am Already ONline")
 
 
 def send_start(bot, update):
